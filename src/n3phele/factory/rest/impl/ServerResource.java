82bb920434609d131f54e8e7e84eefaaadc848d9
LucioC <lucio.cossio@gmail.com>
2013-02-19 14:15:48 -0300

    Added first integration test examples for the rest API.

--- src/n3phele/factory/rest/impl/ServerResource.java
+++ src/n3phele/factory/rest/impl/ServerResource.java
@@ -1,18 +1,15 @@
 package n3phele.factory.rest.impl;
 
 import java.net.URI;
-import java.net.URISyntaxException;
-
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.Response.Status;
 
 import n3phele.factory.hpcloud.FactoryApp;
 import n3phele.factory.hpcloud.HPCloudCredentials;
@@ -28,28 +25,31 @@ public class ServerResource {
 
 	@GET
 	@Produces("text/plain")
-	public String getTest() {
-
+	public String getServers() {
+		//FIXME: implement this correctly, for now is only a test
 		return "Hello World! Test!";
 	}
 
 	@POST
-	@Produces("application/json")
-	@Consumes(MediaType.APPLICATION_JSON)
 	public Response createServer(HPCloudCredentials identity) {
 
 		FactoryApp factory = new FactoryApp(identity.getIdentity(), identity.getSecretKey());
-		factory.run();
 
-		// Return a 201 code
-		URI uri = null;
-		try {
+		try
+		{
+			factory.run();
+
+			// Return a 201 code
+			URI uri = null;
+			//FIXME: should implement this correctly. This is a test.
 			uri = new URI("http://YEY.com/");
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+			return Response.created(uri).build();
 		}
-		return Response.created(uri).build();
+		catch(Exception e)
+		{
+			return Response.status(Status.BAD_REQUEST).build();
+		}
 	}
 
 }
