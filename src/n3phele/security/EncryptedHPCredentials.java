7b59a65d31b2e1b526e2a5b44a3951eab5742382
tinhascheibler <tinhascheibler@gmail.com>
2013-03-22 13:38:05 -0300

    Credentials change

--- src/n3phele/security/EncryptedHPCredentials.java
+++ src/n3phele/security/EncryptedHPCredentials.java
@@ -42,7 +42,7 @@ public class EncryptedHPCredentials {
 	
 	public EncryptedHPCredentials(String passwd)
 	{
-		this.password = "elehp3N" + passwd;
+		this.password = passwd;
 	}
 	
 	public void setCredentials(String encryptedAccessKey, String encryptedSecretKey) throws UnsupportedEncodingException, NoSuchAlgorithmException {
